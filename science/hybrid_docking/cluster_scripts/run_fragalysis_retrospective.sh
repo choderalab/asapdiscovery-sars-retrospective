482e77ccfa173c3b38b0a110bcd686945d0d4648
Alexander Matthew Payne <alex.payne@choderalab.org>
2023-06-06 17:14:11 -0400

    run run_fragalysis_retrospective.sh

--- science/hybrid_docking/cluster_scripts/run_fragalysis_retrospective.sh
+++ science/hybrid_docking/cluster_scripts/run_fragalysis_retrospective.sh
@@ -4,17 +4,17 @@
 #BSUB -eo run_fragalysis_retrospective.stderr
 #BSUB -n 32
 #BSUB -q cpuqueue
-#BSUB -R rusage[mem=8]
+#BSUB -R rusage[mem=4]
 #BSUB -W 24:00
 source ~/.bashrc
 conda activate ad-3.9
 run-docking-oe \
--l /lila/data/chodera/asap-datasets/mpro_fragalysis_2022_10_12/Mpro_combined_labeled_p_only.sdf \
--r '/lila/data/chodera/asap-datasets/current/sars_01_prepped_v3/MPro-P*/*_prepped_receptor_0.oedu' \
+-l /lila/data/chodera/asap-datasets/current/sars_01_prepped_v3/Mpro_combined_labeled_p_only.sdf \
+-r '/lila/data/chodera/asap-datasets/current/sars_01_prepped_v3/Mpro-P*/*_prepped_receptor_0.oedu' \
 -o /lila/data/chodera/asap-datasets/retro_docking/sars_fragalysis_retrospective/20230606_hybrid_p_only \
 -n 32 \
 --omega \
 --relax clash \
-----hybrid
+--hybrid
 echo Done
 date
