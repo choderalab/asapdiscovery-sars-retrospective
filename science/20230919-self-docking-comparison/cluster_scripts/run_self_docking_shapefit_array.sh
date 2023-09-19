d773e997aa828e6ae7a1477666627c5dd70410de
Alexander Matthew Payne <alex.payne@choderalab.org>
2023-09-19 17:54:58 -0400

    cluster script changes

--- science/20230919-self-docking-comparison/cluster_scripts/run_self_docking_shapefit_array.sh
+++ science/20230919-self-docking-comparison/cluster_scripts/run_self_docking_shapefit_array.sh
@@ -9,30 +9,30 @@
 #BSUB -R rusage[mem=4]
 #BSUB -W 2:00
 
-# load file name from array
+## load file name from array
 declare -a cmpd_arr
-cmpds=$(ls /lila/data/chodera/asap-datasets/current/sars_01_prepped_v3/sdf_lsf_array_p_only_by_name/*.sdf | sed -e 's/\.sdf$//')
+cmpds=$(ls /lila/data/chodera/asap-datasets/current/sars_01_prepped_v3/sdf_lsf_array_p_only_by_name/*.sdf | sed -e 's/\/.*\///g' | sed -e 's/\.sdf$//')
 i=0
 for cmpd in $cmpds; do
 cmpd_arr[ $i ]=$cmpd
 (( i++ ))
 done
 
-cmpd=${cmpd_arr[ $LSB_JOBINDEX ]}
+cmpd=${cmpd_arr[ $LSB_JOBINDEX-1]}
 echo $cmpd
 
-#source ~/.bashrc
-#conda activate ad-3.9
-#run-docking-oe \
-#-l "/lila/data/chodera/asap-datasets/current/sars_01_prepped_v3/sdf_lsf_array_p_only/"$LSB_JOBINDEX".sdf" \
-#-r '/lila/data/chodera/asap-datasets/current/sars_01_prepped_v3/Mpro-P*/*_prepped_receptor_0.oedu' \
-#-o /lila/data/chodera/asap-datasets/retro_docking/sars_fragalysis_retrospective/20230608_hybrid_p_only \
-#-n 32 \
-#--omega \
-#--relax clash \
-#--timeout 600 \
-#--max_failures 300 \
-#--hybrid \
-#-log "run_docking_oe."$LSB_JOBINDEX
-#echo Done
-#date
+source ~/.bashrc
+conda activate ad-3.9
+run-docking-oe \
+-l "/lila/data/chodera/asap-datasets/current/sars_01_prepped_v3/sdf_lsf_array_p_only_by_name/"$cmpd".sdf" \
+-r '/lila/data/chodera/asap-datasets/current/sars_01_prepped_v3/*'$cmpd'*/*_prepped_receptor_0.oedu' \
+-o /lila/data/chodera/asap-datasets/retro_docking/sars_fragalysis_retrospective/20230919_self_docked_shapefit_p_only \
+-n 16 \
+--omega \
+--relax clash \
+--timeout 600 \
+--max_failures 300 \
+--shapefit \
+-log "run_docking_oe."$LSB_JOBINDEX
+echo Done
+date
