d05c229a959eff4962e4609276a8da134d5794bf
Alexander Matthew Payne <alexpayne1997@gmail.com>
2023-09-19 17:38:20 -0400

    test named array setup

--- science/20230919-self-docking-comparison/cluster_scripts/run_self_docking_shapefit_array.sh
+++ science/20230919-self-docking-comparison/cluster_scripts/run_self_docking_shapefit_array.sh
@@ -1,25 +1,38 @@
 #!/bin/bash
 ## Example usage:
-## bsub -J "run_fragalysis_retrospective[1-576]" < run_fragalysis_retrospective_array.sh
+## bsub -J "run_self_docking[0-218]" < run_self_docking_shapefit_array.sh
 
-#BSUB -oo run_fragalysis_retrospective_%I.out
-#BSUB -eo run_fragalysis_retrospective_%I.stderr
+#BSUB -oo run_self_docking_%I.out
+#BSUB -eo run_self_docking_%I.stderr
 #BSUB -n 16
 #BSUB -q cpuqueue
 #BSUB -R rusage[mem=4]
 #BSUB -W 2:00
-source ~/.bashrc
-conda activate ad-3.9
-run-docking-oe \
--l "/lila/data/chodera/asap-datasets/current/sars_01_prepped_v3/sdf_lsf_array_p_only/"$LSB_JOBINDEX".sdf" \
--r '/lila/data/chodera/asap-datasets/current/sars_01_prepped_v3/Mpro-P*/*_prepped_receptor_0.oedu' \
--o /lila/data/chodera/asap-datasets/retro_docking/sars_fragalysis_retrospective/20230608_hybrid_p_only \
--n 32 \
---omega \
---relax clash \
---timeout 600 \
---max_failures 300 \
---hybrid \
--log "run_docking_oe."$LSB_JOBINDEX
-echo Done
-date
+
+# load file name from array
+declare -a cmpd_arr
+cmpds=$(ls /lila/data/chodera/asap-datasets/current/sars_01_prepped_v3/sdf_lsf_array_p_only_by_name/*.sdf | sed -e 's/\.sdf$//')
+i=0
+for cmpd in $cmpds; do
+cmpd_arr[ $i ]=$cmpd
+(( i++ ))
+done
+
+cmpd=${cmpd_arr[ $LSB_JOBINDEX ]}
+echo $cmpd
+
+#source ~/.bashrc
+#conda activate ad-3.9
+#run-docking-oe \
+#-l "/lila/data/chodera/asap-datasets/current/sars_01_prepped_v3/sdf_lsf_array_p_only/"$LSB_JOBINDEX".sdf" \
+#-r '/lila/data/chodera/asap-datasets/current/sars_01_prepped_v3/Mpro-P*/*_prepped_receptor_0.oedu' \
+#-o /lila/data/chodera/asap-datasets/retro_docking/sars_fragalysis_retrospective/20230608_hybrid_p_only \
+#-n 32 \
+#--omega \
+#--relax clash \
+#--timeout 600 \
+#--max_failures 300 \
+#--hybrid \
+#-log "run_docking_oe."$LSB_JOBINDEX
+#echo Done
+#date
