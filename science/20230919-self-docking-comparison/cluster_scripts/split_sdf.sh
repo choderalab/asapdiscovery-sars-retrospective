00bee2d5783c25dc0a965b78f603aed368f19dc1
Alexander Matthew Payne <alexpayne1997@gmail.com>
2023-09-19 17:03:35 -0400

    add new cluster scripts for self-docking

---
+++ science/20230919-self-docking-comparison/cluster_scripts/split_sdf.sh
@@ -0,0 +1,5 @@
+split-sdf \
+-i /Users/alexpayne/lilac-mount-point/asap-datasets/current/sars_01_prepped_v3/Mpro_combined_labeled_p_only.sdf \
+-o /Users/alexpayne/lilac-mount-point/asap-datasets/current/sars_01_prepped_v3/sdf_lsf_array_p_only_by_name \
+-c 1 \
+--name_convention name
