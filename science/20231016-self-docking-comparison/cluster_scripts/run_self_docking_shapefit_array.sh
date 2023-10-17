e28260b4ad85e9c2f102feb85c19b55066a27aea
Alexander Matthew Payne <alex.payne@choderalab.org>
2023-10-17 10:14:06 -0400

    update scripts for 2d hybrid and shapefit self docking

--- science/20231016-self-docking-comparison/cluster_scripts/run_self_docking_shapefit_array.sh
+++ science/20231016-self-docking-comparison/cluster_scripts/run_self_docking_shapefit_array.sh
@@ -2,8 +2,8 @@
 ## Example usage:
 ## bsub -J "run_self_docking[1-219]" < run_self_docking_shapefit_array.sh
 
-#BSUB -oo run_self_docking_shapefit_%I.out
-#BSUB -eo run_self_docking_shapefit_%I.stderr
+#BSUB -oo run_self_docking_shapefit_2d_%I.out
+#BSUB -eo run_self_docking_shapefit_2d_%I.stderr
 #BSUB -n 1
 #BSUB -q cpuqueue
 #BSUB -R rusage[mem=4]
@@ -11,7 +11,7 @@
 
 ## load file name from array
 declare -a cmpd_arr
-cmpds=$(ls /lila/data/chodera/asap-datasets/current/sars_01_prepped_v3/sdf_lsf_array_p_only_by_name/*.sdf | sed -e 's/\/.*\///g' | sed -e 's/\.sdf$//')
+cmpds=$(ls /lila/data/chodera/asap-datasets/current/sars_01_prepped_v3/sdf_lsf_array_p_only_by_name_2d/*.sdf | sed -e 's/\/.*\///g' | sed -e 's/\.sdf$//')
 i=0
 for cmpd in $cmpds; do
 cmpd_arr[ $i ]=$cmpd
@@ -24,9 +24,9 @@ echo $cmpd
 source ~/.bashrc
 conda activate ad-3.9
 run-docking-oe \
--l "/lila/data/chodera/asap-datasets/current/sars_01_prepped_v3/sdf_lsf_array_p_only_by_name/"$cmpd".sdf" \
+-l "/lila/data/chodera/asap-datasets/current/sars_01_prepped_v3/sdf_lsf_array_p_only_by_name_2d/"$cmpd".sdf" \
 -r '/lila/data/chodera/asap-datasets/current/sars_01_prepped_v3/*'$cmpd'*/*_prepped_receptor_0.oedu' \
--o /lila/data/chodera/asap-datasets/retro_docking/sars_fragalysis_retrospective/20230919_self_docked_shapefit_p_only \
+-o /lila/data/chodera/asap-datasets/retro_docking/sars_fragalysis_retrospective/20231016_self_docked_shapefit_p_only_2d \
 -n 1 \
 --docking_sys posit \
 --omega \
