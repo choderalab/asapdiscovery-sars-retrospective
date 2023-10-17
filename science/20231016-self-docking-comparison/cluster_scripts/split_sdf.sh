f234c1abc11e5740e5e6194e560d4d97926dfc77
Alexander Matthew Payne <alex.payne@choderalab.org>
2023-10-17 10:14:06 -0400

    update scripts for 2d hybrid and shapefit self docking

--- science/20231016-self-docking-comparison/cluster_scripts/split_sdf.sh
+++ science/20231016-self-docking-comparison/cluster_scripts/split_sdf.sh
@@ -1,5 +1,6 @@
 split-sdf \
 -i ~/asap-datasets/current/sars_01_prepped_v3/Mpro_combined_labeled_p_only.sdf \
--o ~/asap-datasets/current/sars_01_prepped_v3/sdf_lsf_array_p_only_by_name \
+-o ~/asap-datasets/current/sars_01_prepped_v3/sdf_lsf_array_p_only_by_name_2d \
 -c 1 \
---name_convention name
+--name_convention name \
+--flatten
