9049b33da4f40351892d13cabc5aafc4c4ba951c
Alexander Matthew Payne <alex.payne@choderalab.org>
2024-04-10 14:39:48 -0400

    fix cluster script typo

--- science/20240403_multi_pose_docking_v2/self_docking/run_self_docking_multiple_poses_array.sh
+++ science/20240403_multi_pose_docking_v2/self_docking/run_self_docking_multiple_poses_array.sh
@@ -3,7 +3,7 @@
 ## bsub -J "run_self_docking_multiple_poses[1-205]" < run_self_docking_multiple_poses_array.sh
 
 #BSUB -oo array_logs/run_self_docking_multiple_poses_%I.out
-#BSUB -eo array_logs/run_self_docking_multiple_poses_%I.out
+#BSUB -eo array_logs/run_self_docking_multiple_poses_%I.stderr
 #BSUB -n 1
 #BSUB -q cpuqueue
 #BSUB -R rusage[mem=4]
@@ -18,10 +18,10 @@ asap-docking cross-docking \
 --allow-retries \
 --structure-selector SelfDockingSelector \
 --fragalysis-dir /data/chodera/asap-datasets/mpro_fragalysis-04-01-24 \
---ligands "/data/chodera/asap-datasets/mpro_fragalysis-04-01-24_curated_cache/combined_split_2d/$LSB_JOBIDEX.sdf" \
+--ligands "/data/chodera/asap-datasets/mpro_fragalysis-04-01-24_curated_cache/combined_split_2d/$LSB_JOBINDEX.sdf" \
 --no-save-to-cache \
 --cache-dir /data/chodera/asap-datasets/mpro_fragalysis-04-01-24_curated_cache \
 --use-only-cache \
---output-dir /lila/data/chodera/asap-datasets/retro_docking/sars_fragalysis_retrospective/20240403_multi_pose_docking_self_docking \
+--output-dir "/lila/data/chodera/asap-datasets/retro_docking/sars_fragalysis_retrospective/20240403_multi_pose_docking_self_docking/$LSB_JOBINDEX.sdf" \
 --overwrite \
 --num-poses 50
