cc6069f79746c8f05a8dead6b691b5bf62b870f2
Alexander Matthew Payne <alexpayne1997@gmail.com>
2024-04-10 14:17:40 -0400

    update split sdf script

--- science/20240403_multi_pose_docking_v2/self_docking/split_input_sdf.sh
+++ science/20240403_multi_pose_docking_v2/self_docking/split_input_sdf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python ../../production/utils/split_sdf.py \
+split-sdf \
 -i /data/chodera/asap-datasets/mpro_fragalysis-04-01-24_curated_cache/combined.sdf \
 -o /data/chodera/asap-datasets/mpro_fragalysis-04-01-24_curated_cache/combined_split_2d \
 -n 1 \
