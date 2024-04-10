d874cea0c73778469206bcfc9574ebb8b65e57bf
Alexander Matthew Payne <alexpayne1997@gmail.com>
2024-04-10 16:43:03 -0400

    update cluster submission script

--- science/20240403_multi_pose_docking_v2/cross_docking/run_cross_docking_multiple_poses_array.sh
+++ science/20240403_multi_pose_docking_v2/cross_docking/run_cross_docking_multiple_poses_array.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 ## Example usage:
-## bsub -J "run_self_docking_multiple_poses[1-205]" < run_self_docking_multiple_poses_array.sh
+## bsub -J "run_cross_docking_multiple_poses[1-205]" < run_cross_docking_multiple_poses_array.sh
 
-#BSUB -oo array_logs/run_self_docking_multiple_poses_%I.out
-#BSUB -eo array_logs/run_self_docking_multiple_poses_%I.stderr
+#BSUB -oo array_logs/run_cross_docking_multiple_poses_%I.out
+#BSUB -eo array_logs/run_cross_docking_multiple_poses_%I.stderr
 #BSUB -n 1
 #BSUB -q cpuqueue
-#BSUB -R rusage[mem=4]
+#BSUB -R rusage[mem=16]
 #BSUB -W 2:00
 
 source ~/.bashrc
@@ -22,6 +22,6 @@ asap-docking cross-docking \
 --no-save-to-cache \
 --cache-dir /data/chodera/asap-datasets/mpro_fragalysis-04-01-24_curated_cache \
 --use-only-cache \
---output-dir "/lila/data/chodera/asap-datasets/retro_docking/sars_fragalysis_retrospective/20240403_multi_pose_docking_self_docking/$LSB_JOBINDEX.sdf" \
+--output-dir "/lila/data/chodera/asap-datasets/retro_docking/sars_fragalysis_retrospective/20240403_multi_pose_docking_cross_docking/$LSB_JOBINDEX.sdf" \
 --overwrite \
 --num-poses 50
