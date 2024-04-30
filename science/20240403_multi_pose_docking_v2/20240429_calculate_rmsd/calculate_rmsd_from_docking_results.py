97f07c70c273d93af6f333883dd338ca05db0c83
Alexander Matthew Payne <alexpayne1997@gmail.com>
2024-04-30 10:33:05 -0400

    add python rmsd calculation script

---
+++ science/20240403_multi_pose_docking_v2/20240429_calculate_rmsd/calculate_rmsd_from_docking_results.py
@@ -0,0 +1,92 @@
+# Description: Calculate RMSD between ligand poses
+from pathlib import Path
+from tqdm import tqdm
+from asapdiscovery.docking.openeye import POSITDockingResults
+from asapdiscovery.data.schema.ligand import Ligand
+import argparse
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description="Calculate RMSD between ligand poses")
+    parser.add_argument(
+        "-d",
+        "--results_dir",
+        type=Path,
+        required=True,
+        help="Path to directory containing docking results",
+    )
+    parser.add_argument(
+        "-l",
+        "--ligand_ref",
+        type=Path,
+        required=True,
+        help="Path to original ligand sdf file. Must be a single 3d structure",
+    )
+    parser.add_argument(
+        "--output_dir", type=str, required=True, help="Path to output file"
+    )
+    return parser.parse_args()
+
+
+def calculate_ligand_rmsd(ref: Ligand, fit: Ligand) -> list[float]:
+    from asapdiscovery.data.backend.openeye import oechem
+
+    fitmol = fit.to_oemol()
+    refmol = ref.to_oemol()
+    nConfs = fit.num_poses
+    vecRmsd = oechem.OEDoubleArray(nConfs)
+    success = oechem.OERMSD(refmol, fitmol, vecRmsd)
+    if not success:
+        print("RMSD calculation failed")
+    fit.set_SD_data({"RMSD": list(vecRmsd)})
+    return fit
+
+
+def make_df_from_docking_results(results=list[POSITDockingResults]):
+    import pandas as pd
+    from asapdiscovery.docking.docking_data_validation import DockingResultCols
+
+    dfs = []
+    for result in results:
+        docking_dict = {}
+        docking_dict["Query_Ligand"] = result.input_pair.ligand.compound_name
+        docking_dict["Reference_Structure"] = (
+            result.input_pair.complex.target.target_name
+        )
+        docking_dict["Reference_Ligand_SMILES"] = (
+            result.input_pair.complex.ligand.smiles
+        )
+        docking_dict[DockingResultCols.SMILES.value] = result.input_pair.ligand.smiles
+        docking_dict[DockingResultCols.DOCKING_CONFIDENCE_POSIT.value] = (
+            result.posed_ligand.conf_tags["docking-confidence-POSIT"]
+        )
+        docking_dict["RMSD"] = result.posed_ligand.conf_tags["RMSD"]
+        docking_dict["Pose_ID"] = result.posed_ligand.conf_tags["Pose_ID"]
+        docking_dict["POSIT_Method"] = result.posed_ligand.conf_tags["_POSIT_method"]
+        docking_dict["Reference_Ligand"] = (
+            result.input_pair.complex.ligand.compound_name
+        )
+
+        dfs.append(pd.DataFrame(docking_dict))
+
+    df = pd.concat(dfs)
+    return df
+
+
+def main():
+    args = get_args()
+    results_dir = args.results_dir
+    ref = Ligand.from_sdf(args.ligand_ref)
+    output_dir = args.output_dir
+
+    json_paths = list(results_dir.glob("docking_results/*/docking_result.json"))
+    results = [
+        POSITDockingResults.from_json_file(json_file) for json_file in json_paths
+    ]
+
+    for result in tqdm(results):
+        posed_lig = result.posed_ligand
+        calculate_ligand_rmsd(ref, posed_lig)
+
+    df = make_df_from_docking_results(results)
+    df.to_csv(output_dir / "docking_results.csv", index=False)
