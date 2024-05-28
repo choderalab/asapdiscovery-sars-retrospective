112cfcdafc50f69b30b2aac5d55d3824caf21349
Alexander Matthew Payne <alex.payne@choderalab.org>
2024-05-28 05:54:06 -0400

    update cluster script

--- science/20240403_multi_pose_docking_v2/20240430_analyze_cross_docking_results/scripts/run_cross_docking_analysis_cluster.sh
+++ science/20240403_multi_pose_docking_v2/20240430_analyze_cross_docking_results/scripts/run_cross_docking_analysis_cluster.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 ## Example usage:
 
+#BSUB -J cross_docking_analysis
 #BSUB -oo logs/cross_docking_analysis.out
 #BSUB -eo logs/cross_docking_analysis.stderr
-#BSUB -n 128
+#BSUB -n 64
 #BSUB -m lt-gpu
 #BSUB -q cpuqueue
 #BSUB -R rusage[mem=1]
 #BSUB -W 2:00
 
 source ~/.bashrc
-conda activate asapdiscovery
+mamba activate harbor
 
 echo Start
 # network device info
-ifconfig
-# dask info
-dask info versions
 ulimit -c 0
 
-python run_full_cross_docking_evaluation.py \
+python3 run_full_cross_docking_evaluation.py \
 --input /home/paynea/asap-datasets/retro_docking/sars_fragalysis_retrospective/20240424_multi_pose_docking_cross_docking/results_csvs/20240503_combined_results_with_data.csv \
 --output analyzed_data/ \
 --date_dict_path /data/chodera/paynea/asapdiscovery-sars-retrospective/science/20240403_multi_pose_docking_v2/20240430_analyze_cross_docking_results/20240503_inputs_analysis/date_dict.json \
