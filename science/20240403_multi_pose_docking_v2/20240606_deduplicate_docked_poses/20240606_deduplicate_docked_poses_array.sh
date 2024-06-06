05d59556efad2d1f961e504cce3ac94a33810239
Alexander Matthew Payne <alexpayne1997@gmail.com>
2024-06-06 15:04:29 -0400

    use job index correctly

--- science/20240403_multi_pose_docking_v2/20240606_deduplicate_docked_poses/20240606_deduplicate_docked_poses_array.sh
+++ science/20240403_multi_pose_docking_v2/20240606_deduplicate_docked_poses/20240606_deduplicate_docked_poses_array.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ## Example usage:
 ## bsub -J deduplicate_docked_poses[1-205] < 20240606_deduplicate_docked_poses_array.sh
-#BSUB -oo logs/deduplicate_docked_poses.out
-#BSUB -eo logs/deduplicate_docked_poses.stderr
+#BSUB -oo logs/deduplicate_docked_poses_%I.out
+#BSUB -eo logs/deduplicate_docked_poses_%I.stderr
 #BSUB -n 64
 #BSUB -m lt-gpu
 #BSUB -q cpuqueue
@@ -10,7 +10,7 @@
 #BSUB -W 2:00
 
 source ~/.bashrc
-mamba activate harbor
+mamba activate asapdiscovery
 
 echo Start
 date
@@ -18,8 +18,8 @@ date
 ulimit -c 0
 
 python3 filter_similar_molecules.py \
---dock-dir "/home/paynea/asap-datasets/retro_docking/sars_fragalysis_retrospective/20240424_multi_pose_docking_cross_docking"%I \
---out-dir "/home/paynea/asap-datasets/retro_docking/sars_fragalysis_retrospective/20240424_multi_pose_docking_cross_docking/20240606_deduplicated_docked_poses/"%I
+--dock-dir "/home/paynea/asap-datasets/retro_docking/sars_fragalysis_retrospective/20240424_multi_pose_docking_cross_docking"$LSB_JOBINDEX \
+--out-dir "/home/paynea/asap-datasets/retro_docking/sars_fragalysis_retrospective/20240424_multi_pose_docking_cross_docking/20240606_deduplicated_docked_poses/"$LSB_JOBINDEX
 
 date
 echo Done
