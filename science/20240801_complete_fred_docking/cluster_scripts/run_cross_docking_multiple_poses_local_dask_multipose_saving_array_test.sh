c8c1ab80dae1f74bcf2c85e2baba283b8f2cc902
Alexander Matthew Payne <alexpayne1997@gmail.com>
2024-08-16 15:00:33 -0400

    update cluster script test

---
+++ science/20240801_complete_fred_docking/cluster_scripts/run_cross_docking_multiple_poses_local_dask_multipose_saving_array_test.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+## Example usage:
+## bsub -J "multiple_poses[1-205]" < run_cross_docking_multiple_poses_local_dask_multipose_saving_array.sh
+
+#BSUB -oo logs/multiple_poses_%I.out
+#BSUB -eo logs/multiple_poses_%I.stderr
+#BSUB -n 8
+#BSUB -m lt-gpu
+#BSUB -q cpuqueue
+#BSUB -R rusage[mem=12]
+#BSUB -W 2:00
+
+source ~/.bashrc
+conda activate asapdiscovery
+
+echo Start
+# network device info
+ifconfig
+# dask info
+dask info versions
+ulimit -c 0
+
+
+asap-docking cross-docking \
+--target SARS-CoV-2-Mpro \
+--use-omega \
+--omega-dense \
+--allow-retries \
+--allow-final-clash \
+--structure-selector PairwiseSelector \
+--fragalysis-dir /data/chodera/asap-datasets/mpro_fragalysis-04-01-24 \
+--ligands "/data/chodera/asap-datasets/mpro_fragalysis-04-01-24_curated_cache/combined_split_2d/$LSB_JOBINDEX.sdf" \
+--no-save-to-cache \
+--cache-dir /data/chodera/asap-datasets/mpro_fragalysis-04-01-24_curated_cache \
+--use-only-cache \
+--output-dir "/lila/data/chodera/asap-datasets/retro_docking/sars_fragalysis_retrospective/20240816_fred_docking/$LSB_JOBINDEX" \
+--no-overwrite \
+--num-poses 50 \
+--use-dask \
+--dask-type local
+
+echo Done
+date
