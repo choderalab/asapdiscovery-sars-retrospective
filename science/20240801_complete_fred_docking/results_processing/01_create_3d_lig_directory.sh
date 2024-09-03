6332a4641b3526e06b2647c6d834bee62ed2c58a
Alexander Matthew Payne <alexpayne1997@gmail.com>
2024-09-03 14:33:34 -0400

    add script for creating cached lig dir

---
+++ science/20240801_complete_fred_docking/results_processing/01_create_3d_lig_directory.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python3 /data/chodera/paynea/asapdiscovery-sars-retrospective/production/covid_moonshot_fragalysis_processing/combined_sdf_from_cache.py \
+--input_cache /data/chodera/asap-datasets/mpro_fragalysis-04-01-24_curated_cache/ \
+--output_dir /data/chodera/asap-datasets/mpro_fragalysis-04-01-24_curated_cache/combined_split_3d_20240903/
