6e4a8615e9820a3eb9eb94c69adedcfec8010cc8
Alexander Matthew Payne <alexpayne1997@gmail.com>
2024-09-05 10:59:01 -0400

    add command for splitting up the 3d files

--- science/20240801_complete_fred_docking/results_processing/01_create_3d_lig_directory.sh
+++ science/20240801_complete_fred_docking/results_processing/01_create_3d_lig_directory.sh
@@ -3,3 +3,9 @@
 python3 /data/chodera/paynea/asapdiscovery-sars-retrospective/production/covid_moonshot_fragalysis_processing/combined_sdf_from_cache.py \
 --input_cache /data/chodera/asap-datasets/mpro_fragalysis-04-01-24_curated_cache/ \
 --output_dir /data/chodera/asap-datasets/mpro_fragalysis-04-01-24_curated_cache/combined_split_3d_20240903/
+
+python3 /data/chodera/paynea/asapdiscovery-sars-retrospective/production/utils/split_sdf.py \
+--sdf_fn /data/chodera/asap-datasets/mpro_fragalysis-04-01-24_curated_cache/combined_split_3d_20240903/combined_3d.sdf \
+--output_dir /data/chodera/asap-datasets/mpro_fragalysis-04-01-24_curated_cache/combined_split_3d_20240903/ \
+--chunk_size 1 \
+--name_convention "integer"
