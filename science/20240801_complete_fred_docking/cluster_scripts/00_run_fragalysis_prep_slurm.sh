ee854189cecba61a17eeb0f1b4cc487df6907b57
Alexander Matthew Payne <alexpayne1997@gmail.com>
2025-01-10 14:32:30 -0500

    use iris paths

--- science/20240801_complete_fred_docking/cluster_scripts/00_run_fragalysis_prep_slurm.sh
+++ science/20240801_complete_fred_docking/cluster_scripts/00_run_fragalysis_prep_slurm.sh
@@ -22,10 +22,10 @@ ulimit -c 0
 # Run ASAP CLI
 asap-cli protein-prep \
   --target SARS-CoV-2-Mpro \
-  --fragalysis-dir /data/chodera/asap-datasets/mpro_fragalysis-04-01-24_p_series \
-  --cache-dir /data/chodera/asap-datasets/mpro_fragalysis-04-01-24_p_series_cache_20250107 \
+  --fragalysis-dir /data1/choderaj/paynea/asap-datasets/mpro_fragalysis-04-01-24_p_series \
+  --cache-dir /data1/choderaj/paynea/asap-datasets/mpro_fragalysis-04-01-24_p_series_cache_20250110 \
   --output-dir fragalysis_prep_out \
-  --loop-db /lila/home/kaminowb/.openeye/rcsb_spruce.loop_db \
+  --loop-db /data1/choderaj/asap-playground/rcsb_spruce.loop_db \
   --save-to-cache \
   --ref-chain A \
   --active-site-chain A \
