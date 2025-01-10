2b573fc6223d8b7cf689fa1b44040dd6cb893e0e
Alexander Matthew Payne <alexpayne1997@gmail.com>
2025-01-10 12:03:27 -0500

    add slurm script for iris

---
+++ science/20240801_complete_fred_docking/cluster_scripts/00_run_fragalysis_prep_slurm.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+## Example usage:
+#SBATCH --job-name="fragalysis_prep"
+#SBATCH --output="logs/fragalysis_prep.out"
+#SBATCH --error="logs/fragalysis_prep.stderr"
+#SBATCH --ntasks=64
+#SBATCH --partition=cpuqueue
+#SBATCH --mem=2GB
+#SBATCH --time=02:00:00
+
+# Activate conda environment
+source ~/.bashrc
+conda activate asap2025
+
+echo "Start"
+# Network device info
+ifconfig
+# Dask info
+dask info versions
+ulimit -c 0
+
+# Run ASAP CLI
+asap-cli protein-prep \
+  --target SARS-CoV-2-Mpro \
+  --fragalysis-dir /data/chodera/asap-datasets/mpro_fragalysis-04-01-24_p_series \
+  --cache-dir /data/chodera/asap-datasets/mpro_fragalysis-04-01-24_p_series_cache_20250107 \
+  --output-dir fragalysis_prep_out \
+  --loop-db /lila/home/kaminowb/.openeye/rcsb_spruce.loop_db \
+  --save-to-cache \
+  --ref-chain A \
+  --active-site-chain A \
+
+echo "Done"
+date
