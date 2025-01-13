cabc668f2abe6c9d846f3a8746feb84106c075cb
Alexander Matthew Payne <alexpayne1997@gmail.com>
2025-01-13 14:50:31 -0500

    update slurm script

--- science/20240801_complete_fred_docking/prep_and_docking_scripts/02_run_cross_docking_multiple_poses_fred_array_slurm.sh
+++ science/20240801_complete_fred_docking/prep_and_docking_scripts/02_run_cross_docking_multiple_poses_fred_array_slurm.sh
@@ -26,11 +26,12 @@ asap-docking cross-docking \
   --omega-dense \
   --allow-retries \
   --allow-final-clash \
+  --relax-mode clash \
   --posit-method FRED \
   --structure-selector PairwiseSelector \
-  --fragalysis-dir /data1/choderaj/paynea/asap-datasets/mpro_fragalysis-04-01-24_p_series \
-  --ligands "/data/chodera/asap-datasets/mpro_fragalysis-04-01-24_curated_cache/combined_split_2d/${SLURM_ARRAY_TASK_ID}.sdf" \
-  --cache-dir /data1/choderaj/paynea/asap-datasets/mpro_fragalysis-04-01-24_p_series_cache_20250110 \
+  --fragalysis-dir mpro_fragalysis-04-01-24_p_series_curated_20250113 \
+  --ligands "/data1/choderaj/paynea/asap-datasets/mpro_fragalysis-04-01-24_p_series_curated_cache_20250113/split_2d/${SLURM_ARRAY_TASK_ID}.sdf" \
+  --cache-dir /data1/choderaj/paynea/asap-datasets/mpro_fragalysis-04-01-24_p_series_curated_cache_20250113 \
   --output-dir "/data1/choderaj/paynea/asap-datasets/20250113_fred_docking/${SLURM_ARRAY_TASK_ID}" \
   --no-overwrite \
   --no-save-to-cache \
