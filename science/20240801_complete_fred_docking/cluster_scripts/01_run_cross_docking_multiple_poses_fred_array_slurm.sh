830085c2f8a6d9c5a98e8d36cabedf776d3bb598
Alexander Matthew Payne <alexpayne1997@gmail.com>
2025-01-13 11:06:43 -0500

    add iris ligand cache splitting script

--- science/20240801_complete_fred_docking/cluster_scripts/01_run_cross_docking_multiple_poses_fred_array_slurm.sh
+++ science/20240801_complete_fred_docking/cluster_scripts/01_run_cross_docking_multiple_poses_fred_array_slurm.sh
@@ -28,10 +28,10 @@ asap-docking cross-docking \
   --allow-final-clash \
   --posit-method FRED \
   --structure-selector PairwiseSelector \
-  --fragalysis-dir /data/chodera/asap-datasets/mpro_fragalysis-04-01-24_p_series \
+  --fragalysis-dir /data1/choderaj/paynea/asap-datasets/mpro_fragalysis-04-01-24_p_series \
   --ligands "/data/chodera/asap-datasets/mpro_fragalysis-04-01-24_curated_cache/combined_split_2d/${SLURM_ARRAY_TASK_ID}.sdf" \
-  --cache-dir /data/chodera/asap-datasets/mpro_fragalysis-04-01-24_curated_cache_p_series \
-  --output-dir "/lila/data/chodera/asap-datasets/retro_docking/sars_fragalysis_retrospective/20241018_fred_docking/${SLURM_ARRAY_TASK_ID}" \
+  --cache-dir /data1/choderaj/paynea/asap-datasets/mpro_fragalysis-04-01-24_p_series_cache_20250110 \
+  --output-dir "/data1/choderaj/paynea/asap-datasets/20250113_fred_docking/${SLURM_ARRAY_TASK_ID}" \
   --no-overwrite \
   --no-save-to-cache \
   --use-only-cache \
