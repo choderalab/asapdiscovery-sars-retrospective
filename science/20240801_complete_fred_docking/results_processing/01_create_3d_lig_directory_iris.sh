f629df483a4c57a5c73014e6eb58d939a40b62f7
Alexander Matthew Payne <alexpayne1997@gmail.com>
2025-01-13 11:15:31 -0500

    update lig directory creation

--- science/20240801_complete_fred_docking/results_processing/01_create_3d_lig_directory_iris.sh
+++ science/20240801_complete_fred_docking/results_processing/01_create_3d_lig_directory_iris.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+# save 3d
 python3 /data1/choderaj/paynea/asapdiscovery-sars-retrospective/production/covid_moonshot_fragalysis_processing/combined_sdf_from_cache.py \
 --input_cache /data1/choderaj/paynea/asap-datasets/mpro_fragalysis-04-01-24_p_series_cache_20250110 \
 --output_dir /data1/choderaj/paynea/asap-datasets/mpro_fragalysis-04-01-24_p_series_cache_20250110/combined_split_3d_20250113/
+
+# save 2d
+python3 /data1/choderaj/paynea/asapdiscovery-sars-retrospective/production/covid_moonshot_fragalysis_processing/combined_sdf_from_cache.py \
+--input_cache /data1/choderaj/paynea/asap-datasets/mpro_fragalysis-04-01-24_p_series_cache_20250110 \
+--output_dir /data1/choderaj/paynea/asap-datasets/mpro_fragalysis-04-01-24_p_series_cache_20250110/combined_split_3d_20250113/ \
+--flatten
 
 python3 /data1/choderaj/paynea/asapdiscovery-sars-retrospective/production/utils/split_sdf.py \
 --sdf_fn /data1/choderaj/paynea/asap-datasets/mpro_fragalysis-04-01-24_p_series_cache_20250110/combined_split_3d_20250113/combined_3d.sdf \
