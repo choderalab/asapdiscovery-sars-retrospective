8b21d9d1c80010d032100f516494b204f5f0ec7c
Alexander Matthew Payne <alexpayne1997@gmail.com>
2025-01-22 11:50:32 -0500

    move and update combine and process results script

---
+++ science/20240801_complete_fred_docking/prep_and_docking_scripts/04_combine_and_process_results.sh
@@ -0,0 +1,18 @@
+#!bin/bash
+#python3 03_combine_and_process_results.py \
+#-r /Users/alexpayne/Scientific_Projects/mers-drug-discovery/sars2-retrospective-analysis/20241018_fred_docking/rmsd_csvs/ \
+#-c /Users/alexpayne/Scientific_Projects/mers-drug-discovery/sars2-retrospective-analysis/mpro_fragalysis-04-01-24_curated_cache_p_series \
+#-d /Users/alexpayne/Scientific_Projects/asapdiscovery-sars-retrospective/science/20240403_multi_pose_docking_v2/20240430_analyze_cross_docking_results/20240503_inputs_analysis \
+#-o '20241205_combined_results_with_data.csv'
+#python3 03_combine_and_process_results.py \
+#-r /data/chodera/asap-datasets/retro_docking/sars_fragalysis_retrospective/20241018_fred_docking/rmsd_csvs \
+#-c /data/chodera/asap-datasets/mpro_fragalysis-04-01-24_curated_cache_p_series \
+#-d /data/chodera/paynea/asapdiscovery-sars-retrospective/science/20240403_multi_pose_docking_v2/20240430_analyze_cross_docking_results/20240503_inputs_analysis \
+#-o /data/chodera/asap-datasets/retro_docking/sars_fragalysis_retrospective/20241018_fred_docking/rmsd_csvs/20241205_combined_results_with_data.csv
+source ~/.bashrc
+conda activate asap2025e
+python3 04_combine_and_process_results.py \
+-r /data1/choderaj/paynea/asap-datasets/20250113_fred_docking/rmsc_csvs \
+-c /data1/choderaj/paynea/asap-datasets/mpro_fragalysis-04-01-24_p_series_curated_cache_20250113 \
+-d /data1/choderaj/paynea/asapdiscovery-sars-retrospective/science/20240403_multi_pose_docking_v2/20240430_analyze_cross_docking_results/20240503_inputs_analysis \
+-o /data1/choderaj/paynea/asap-datasets/20250113_fred_docking/rmsc_csvs/20241205_combined_results_with_data.csv
