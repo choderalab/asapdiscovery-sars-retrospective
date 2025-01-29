d411b7ebc88460878e81361ed3751a526b78dcca
Alex Payne <alex.payne@choderalab.org>
2025-01-29 15:43:00 -0500

    update slurm script

--- science/20240801_complete_fred_docking/analysis/05_run_cross_docking_analysis_slurm.sh
+++ science/20240801_complete_fred_docking/analysis/05_run_cross_docking_analysis_slurm.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=cross_docking_analysis
 #SBATCH --output=logs/cross_docking_analysis_%A_%a.out
 #SBATCH --error=logs/cross_docking_analysis_%A_%a.out
-#SBATCH --cpus-per-task 64
+#SBATCH --cpus-per-task 32
 #SBATCH --partition=cpu
 #SBATCH --mem=128GB
 #SBATCH --time=2:00:00
@@ -18,7 +18,7 @@ ulimit -c 0
 python3 05_run_cross_docking_analysis.py \
 --input /data1/choderaj/paynea/asap-datasets/20250113_fred_docking/rmsd_csvs/20250122_combined_results_with_data.csv \
 --output /data1/choderaj/paynea/asap-datasets/20250113_fred_docking/analyzed_data/ \
---n_cpus 64
+--n_cpus 32
 
 date
 echo Done
