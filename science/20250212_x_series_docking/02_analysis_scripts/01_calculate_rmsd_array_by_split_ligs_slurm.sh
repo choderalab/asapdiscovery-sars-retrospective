794f6436947fac447cf718f63185b0aa7ee9f450
Alexander Matthew Payne <alexpayne1997@gmail.com>
2025-02-17 13:45:04 -0500

    update rmsd calculation script

--- science/20250212_x_series_docking/02_analysis_scripts/01_calculate_rmsd_array_by_split_ligs_slurm.sh
+++ science/20250212_x_series_docking/02_analysis_scripts/01_calculate_rmsd_array_by_split_ligs_slurm.sh
@@ -13,7 +13,7 @@ source ~/.bashrc
 conda activate asap2025e
 
 # don't forget to use the 3d ligands instead of the 2d
-python 03_calculate_rmsd_from_docking_results.py \
+python 01_calculate_rmsd_from_docking_results.py \
 -d "/data1/choderaj/paynea/asap-datasets/20250212_p_to_x_fred/${SLURM_ARRAY_TASK_ID}" \
 -l "/data1/choderaj/paynea/asap-datasets/mpro_fragalysis-04-01-24_p_series_curated_cache_20250113/ligand_files_20250113/split_3d/${SLURM_ARRAY_TASK_ID}.sdf" \
 -o "/data1/choderaj/paynea/asap-datasets/20250212_p_to_x_fred/rmsd_csvs/${SLURM_ARRAY_TASK_ID}.csv"
