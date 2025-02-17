9c34c2e2239e005dea6d2f7ec9db14e60d93c1c5
Alexander Matthew Payne <alexpayne1997@gmail.com>
2025-02-17 16:26:52 -0500

    generate date_dict script

---
+++ science/20250212_x_series_docking/02_analysis_scripts/02_generate_new_date_dict.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=generate_date_dict
+#SBATCH --output=logs/generate_date_dict_%A_%a.out
+#SBATCH --error=logs/generate_date_dict_%A_%a.err
+#SBATCH --cpus-per-task 1
+#SBATCH --partition=cpu
+#SBATCH --mem=64GB
+#SBATCH --time=00:30:00
+
+source ~/.bashrc
+conda activate asap2025e
+python3 /data1/choderaj/paynea/asapdiscovery-sars-retrospective/production/covid_moonshot_fragalysis_processing/generate_date_dict.py \
+--input-dir /data1/choderaj/paynea/asap-datasets/mpro_fragalysis-04-01-24_x_series_active_site \
+--output-dir /data1/choderaj/paynea/asapdiscovery-sars-retrospective/data/cmpd_date_dict
