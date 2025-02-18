94daa14ab7bfcdb41e73379d17bfe85418acc782
Alexander Matthew Payne <alexpayne1997@gmail.com>
2025-02-18 15:03:32 -0500

    simplify tanimoto calculation

--- science/20250212_x_series_docking/02_analysis_scripts/03_calculate_ecfp_tanimoto.sh
+++ science/20250212_x_series_docking/02_analysis_scripts/03_calculate_ecfp_tanimoto.sh
@@ -7,6 +7,6 @@
 #SBATCH --time=02:00:00
 source ~/.bashrc
 mamba activate asap2025e
-python3 03_run_chemical_similarity_analysis.py \
+python3 03_calculate_ecfp_tanimoto.py \
 --ref-ligand-sdf /data1/choderaj/paynea/asap-datasets/mpro_fragalysis-04-01-24_p_series_curated_cache_20250113/ligand_files_20250113/combined_3d.sdf \
 --output-dir /data1/choderaj/paynea/asap-datasets/mpro_fragalysis-04-01-24_p_series_curated_cache_20250113/ligand_files_20250113
