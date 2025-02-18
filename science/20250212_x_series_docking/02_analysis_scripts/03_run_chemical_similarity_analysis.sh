8cc26bfda5508f461bea5162bc0df3202ca7579f
Alexander Matthew Payne <alexpayne1997@gmail.com>
2025-02-18 14:32:55 -0500

    make intermediates closer

--- science/20250212_x_series_docking/02_analysis_scripts/03_run_chemical_similarity_analysis.sh
+++ science/20250212_x_series_docking/02_analysis_scripts/03_run_chemical_similarity_analysis.sh
@@ -10,4 +10,6 @@ source ~/.bashrc
 mamba activate asap2025e
 python3 03_run_chemical_similarity_analysis.py \
 --ref-ligand-sdf /data1/choderaj/paynea/asap-datasets/mpro_fragalysis-04-01-24_p_series_curated_cache_20250113/ligand_files_20250113/combined_3d.sdf \
---output-dir /data1/choderaj/paynea/asap-datasets/mpro_fragalysis-04-01-24_p_series_curated_cache_20250113/ligand_files_20250113/cpu32
+--output-dir /data1/choderaj/paynea/asap-datasets/mpro_fragalysis-04-01-24_p_series_curated_cache_20250113/ligand_files_20250113/cpu32 \
+--resume \
+--n-processes 32
