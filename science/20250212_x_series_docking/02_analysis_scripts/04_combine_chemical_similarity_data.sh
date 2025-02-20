e3d43646e9f01c41e6b6d9e75712496b052ee94a
Alexander Matthew Payne <alexpayne1997@gmail.com>
2025-02-20 16:44:12 -0500

    combine chemical similarity data

--- science/20250212_x_series_docking/02_analysis_scripts/04_combine_chemical_similarity_data.sh
+++ science/20250212_x_series_docking/02_analysis_scripts/04_combine_chemical_similarity_data.sh
@@ -8,7 +8,7 @@
 #SBATCH --time=02:00:00
 source ~/.bashrc
 mamba activate asap2025e
-python 04_04_combine_chemical_similarity_data.py \
+python 04_combine_chemical_similarity_data.py \
 /data1/choderaj/paynea/asap-datasets/mpro_fragalysis-04-01-24_p_series_curated_cache_20250113/ligand_files_20250113/tanimoto_combo_p_to_x/tanimoto_combo.csv \
 /data1/choderaj/paynea/asap-datasets/mpro_fragalysis-04-01-24_p_series_curated_cache_20250113/ligand_files_20250113/mcs_tanimoto_p_to_x/mcs_tanimoto.csv \
 /data1/choderaj/paynea/asap-datasets/mpro_fragalysis-04-01-24_p_series_curated_cache_20250113/ligand_files_20250113/ecfp_tanimoto_p_to_x/fingerprint_similarities.csv
