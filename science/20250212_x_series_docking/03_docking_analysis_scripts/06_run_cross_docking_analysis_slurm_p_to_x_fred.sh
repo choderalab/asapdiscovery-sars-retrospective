d78289cfbbbf80309100a0890586717e090ea029
Alexander Matthew Payne <alexpayne1997@gmail.com>
2025-02-26 16:40:31 -0500

    add docking analysis

---
+++ science/20250212_x_series_docking/03_docking_analysis_scripts/06_run_cross_docking_analysis_slurm_p_to_x_fred.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name=cross_docking_analysis
+#SBATCH --output=logs/cross_docking_analysis_%A.out
+#SBATCH --error=logs/cross_docking_analysis_%A.out
+#SBATCH --cpus-per-task 32
+#SBATCH --partition=cpu
+#SBATCH --mem=128GB
+#SBATCH --time=4:00:00
+
+source ~/.bashrc
+conda activate harbor
+
+echo Start
+date
+# network device info
+ulimit -c 0
+
+python3 05_run_cross_docking_analysis.py \
+--input /data1/choderaj/paynea/asap-datasets/20250212_p_to_x_fred/rmsd_csvs/20250217_combined_results_with_data.csv \
+--output /data1/choderaj/paynea/asap-datasets/20250212_p_to_x_fred/analyzed_data \
+--date_dict_path /data1/choderaj/paynea/asapdiscovery-sars-retrospective/data/cmpd_date_dict/date_dict.json \
+--n_cpus 32
+
+date
+echo Done
