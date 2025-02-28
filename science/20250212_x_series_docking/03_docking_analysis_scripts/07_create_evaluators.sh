92b28e0cb28d6c9c8aad8f1567d54e99c4d52545
Alexander Matthew Payne <alexpayne1997@gmail.com>
2025-02-28 16:33:56 -0500

    update cross docking evaluators to be serializeable

--- science/20250212_x_series_docking/03_docking_analysis_scripts/07_create_evaluators.sh
+++ science/20250212_x_series_docking/03_docking_analysis_scripts/07_create_evaluators.sh
@@ -17,7 +17,7 @@ ulimit -c 0
 
 python3 07_create_evaluators.sh \
 --input /data1/choderaj/paynea/asap-datasets/20250212_p_to_x_posit/rmsd_csvs/20250217_combined_results_with_data.csv \
---output /data1/choderaj/paynea/asap-datasets/20250212_p_to_x_posit/analyzed_data \
+--output /data1/choderaj/paynea/asap-datasets/20250212_p_to_x_posit/test_evaluator_creation \
 --update-n-per-split
 
 date
