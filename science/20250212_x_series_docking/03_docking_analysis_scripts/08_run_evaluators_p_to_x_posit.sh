543d6e4a9a7e52357e16931153bb891bbce69529
Alexander Matthew Payne <alexpayne1997@gmail.com>
2025-03-13 15:25:19 -0400

    use model dump instead of dict

--- science/20250212_x_series_docking/03_docking_analysis_scripts/08_run_evaluators_p_to_x_posit.sh
+++ science/20250212_x_series_docking/03_docking_analysis_scripts/08_run_evaluators_p_to_x_posit.sh
@@ -16,12 +16,20 @@ date
 
 data_dir='/data1/choderaj/paynea/asap-datasets/20250212_p_to_x_posit'
 
+#python3 08_run_evaluators.py \
+#--input $data_dir/combined_results/20250311_combined_results \
+#--output $data_dir/similarity_split \
+#--n-cpus 4 \
+#--job-id $SLURM_ARRAY_TASK_ID \
+#--evaluator-json $data_dir/similarity_split/evaluator_*"${SLURM_ARRAY_TASK_ID}".json
+
 python3 08_run_evaluators.py \
 --input $data_dir/combined_results/20250311_combined_results \
---output $data_dir/similarity_split \
+--output $data_dir/scaffold_split \
 --n-cpus 4 \
 --job-id $SLURM_ARRAY_TASK_ID \
---evaluator-json $data_dir/similarity_split/evaluator_*"${SLURM_ARRAY_TASK_ID}".json
+--evaluator-json $data_dir/scaffold_split/evaluator_*"${SLURM_ARRAY_TASK_ID}".json
+
 
 date
 echo Done
