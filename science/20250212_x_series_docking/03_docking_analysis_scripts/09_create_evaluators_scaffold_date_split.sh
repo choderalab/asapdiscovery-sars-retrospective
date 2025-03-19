a6a40453a64299b22bc09825538813d780230cd0
Alexander Matthew Payne <alexpayne1997@gmail.com>
2025-03-19 16:52:36 -0400

    use correct create evaluator script

--- science/20250212_x_series_docking/03_docking_analysis_scripts/09_create_evaluators_scaffold_date_split.sh
+++ science/20250212_x_series_docking/03_docking_analysis_scripts/09_create_evaluators_scaffold_date_split.sh
@@ -17,7 +17,7 @@ ulimit -c 0
 
 data_dir='/data1/choderaj/paynea/asap-datasets/20250212_p_to_x_posit'
 
-python3 09_create_evaluators.py \
+python3 09_create_evaluators_v2.py \
 --input $data_dir/combined_results/20250311_combined_results \
 --output $data_dir/scaffold_split \
 --settings 'settings_scaffold_split_not_x_to_x.yml' \
