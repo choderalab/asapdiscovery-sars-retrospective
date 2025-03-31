4ea6399db4d4ea2fd85a32909c4e656c6106f723
Alexander Matthew Payne <alexpayne1997@gmail.com>
2025-03-31 14:06:07 -0400

    add analysis scripts

---
+++ science/20250212_x_series_docking/02_chemical_similarity_analysis_scipts/04_combine_chemical_similarity_data_p_and_x.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=combine_chemical_similarity_data
+#SBATCH --output=logs/combine_chemical_similarity_data.out
+#SBATCH --error=logs/combine_chemical_similarity_data.err
+#SBATCH --partition=cpu
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=64GB
+#SBATCH --time=02:00:00
+source ~/.bashrc
+mamba activate asap2025e
+python 04_combine_chemical_similarity_data.py \
+/data1/choderaj/paynea/asap-datasets/mpro_fragalysis-04-01-24_p_series_curated_cache_20250113/ligand_files_20250113/tanimoto_combo_p_to_x/tanimoto_combo.csv \
+/data1/choderaj/paynea/asap-datasets/mpro_fragalysis-04-01-24_p_series_curated_cache_20250113/ligand_files_20250113/tanimoto_combo_p_to_p/tanimoto_combo.csv \
+/data1/choderaj/paynea/asap-datasets/mpro_fragalysis-04-01-24_x_series_active_site_cache_20250212/ligand_files_20250212/tanimoto_combo_x_to_x/tanimoto_combo.csv \
+/data1/choderaj/paynea/asap-datasets/mpro_fragalysis-04-01-24_p_series_curated_cache_20250113/ligand_files_20250113/mcs_tanimoto_p_to_x/mcs_tanimoto.csv \
+/data1/choderaj/paynea/asap-datasets/mpro_fragalysis-04-01-24_x_series_active_site_cache_20250212/ligand_files_20250212/mcs_tanimoto/mcs_tanimoto.csv  \
+/data1/choderaj/paynea/asap-datasets/mpro_fragalysis-04-01-24_p_series_curated_cache_20250113/ligand_files_20250113/mcs_tanimoto/mcs_tanimoto.csv \
+/data1/choderaj/paynea/asap-datasets/mpro_fragalysis-04-01-24_p_series_curated_cache_20250113/ligand_files_20250113/ecfp_tanimoto_p_to_x/fingerprint_similarities.csv \
+/data1/choderaj/paynea/asap-datasets/mpro_fragalysis-04-01-24_p_series_curated_cache_20250113/ligand_files_20250113/ecfp_tanimoto/fingerprint_similarities.csv \
+/data1/choderaj/paynea/asap-datasets/mpro_fragalysis-04-01-24_x_series_active_site_cache_20250212/ligand_files_20250212/ecfp_tanimoto/fingerprint_similarities.csv \
+--output-dir /data1/choderaj/paynea/asap-datasets/mpro_fragalysis-04-01-24_p_series_curated_cache_20250113/ligand_files_20250113/combined_p_and_x_chemical_similarity_data
