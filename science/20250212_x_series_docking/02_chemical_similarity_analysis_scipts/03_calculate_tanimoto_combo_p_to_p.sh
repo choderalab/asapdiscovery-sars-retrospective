2a83337c7fda3f4047de6c2d0a66d9d2e8f3fdbf
Alex Payne <alex.payne@choderalab.org>
2025-04-01 10:33:13 -0400

    update slurm script

--- science/20250212_x_series_docking/02_chemical_similarity_analysis_scipts/03_calculate_tanimoto_combo_p_to_p.sh
+++ science/20250212_x_series_docking/02_chemical_similarity_analysis_scipts/03_calculate_tanimoto_combo_p_to_p.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=calculate_tanimoto_combo
-#SBATCH --output=logs/calculate_tanimoto_combo.out
-#SBATCH --error=logs/calculate_tanimoto_combo.err
+#SBATCH --output=logs/calculate_tanimoto_combo_p_to_p.out
+#SBATCH --error=logs/calculate_tanimoto_combo_p_to_p.err
 #SBATCH --partition=cpu
 #SBATCH --cpus-per-task=32
 #SBATCH --mem=64GB
@@ -9,6 +9,6 @@
 source ~/.bashrc
 mamba activate asap2025e
 python3 03_calculate_tanimoto_combo.py \
---ref-ligand-sdf /data1/choderaj/paynea/asap-datasets/mpro_fragalysis-04-01-24_p_series_curated_cache_20250113/ligand_files_20250212/combined_3d.sdf \
+--ref-ligand-sdf /data1/choderaj/paynea/asap-datasets/mpro_fragalysis-04-01-24_p_series_curated_cache_20250113/ligand_files_20250113/combined_3d.sdf \
 --query-ligand-sdf /data1/choderaj/paynea/asap-datasets/mpro_fragalysis-04-01-24_p_series_curated_cache_20250113/ligand_files_20250113/combined_3d.sdf \
 --output-dir /data1/choderaj/paynea/asap-datasets/mpro_fragalysis-04-01-24_p_series_curated_cache_20250113/ligand_files_20250113/tanimoto_combo_p_to_p
