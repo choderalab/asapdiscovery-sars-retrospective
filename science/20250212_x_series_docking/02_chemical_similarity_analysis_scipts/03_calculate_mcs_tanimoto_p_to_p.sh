22b84443aa0ee8004e602ea0ecba2c87ee9db024
Alexander Matthew Payne <alexpayne1997@gmail.com>
2025-04-03 14:57:01 -0400

    add parallelization to mcs tanimoto calculation

--- science/20250212_x_series_docking/02_chemical_similarity_analysis_scipts/03_calculate_mcs_tanimoto_p_to_p.sh
+++ science/20250212_x_series_docking/02_chemical_similarity_analysis_scipts/03_calculate_mcs_tanimoto_p_to_p.sh
@@ -3,10 +3,12 @@
 #SBATCH --output=logs/calculate_mcs_tanimoto.out
 #SBATCH --error=logs/calculate_mcs_tanimoto.err
 #SBATCH --partition=cpu
+#SBATCH --cpus-per-task=32
 #SBATCH --mem=64GB
 #SBATCH --time=02:00:00
 source ~/.bashrc
 mamba activate asap2025e
 python3 03_calculate_mcs_tanimoto.py \
 --ref-ligand-sdf /data1/choderaj/paynea/asap-datasets/mpro_fragalysis-04-01-24_p_series_curated_cache_20250113/ligand_files_20250113/combined_3d.sdf \
---output-dir /data1/choderaj/paynea/asap-datasets/mpro_fragalysis-04-01-24_p_series_curated_cache_20250113/ligand_files_20250113/mcs_tanimoto
+--output-dir /data1/choderaj/paynea/asap-datasets/mpro_fragalysis-04-01-24_p_series_curated_cache_20250113/ligand_files_20250113/mcs_tanimoto \
+--ncpus 32
--- science/20250212_x_series_docking/02_chemical_similarity_analysis_scipts/03_calculate_mcs_tanimoto_p_to_x.sh
+++ science/20250212_x_series_docking/02_chemical_similarity_analysis_scipts/03_calculate_mcs_tanimoto_p_to_x.sh
@@ -11,4 +11,5 @@ mamba activate asap2025e
 python3 03_calculate_mcs_tanimoto.py \
 --ref-ligand-sdf /data1/choderaj/paynea/asap-datasets/mpro_fragalysis-04-01-24_x_series_active_site_cache_20250212/ligand_files_20250212/combined_3d.sdf \
 --query-ligand-sdf /data1/choderaj/paynea/asap-datasets/mpro_fragalysis-04-01-24_p_series_curated_cache_20250113/ligand_files_20250113/combined_3d.sdf \
---output-dir /data1/choderaj/paynea/asap-datasets/mpro_fragalysis-04-01-24_p_series_curated_cache_20250113/ligand_files_20250113/mcs_tanimoto_p_to_x
+--output-dir /data1/choderaj/paynea/asap-datasets/mpro_fragalysis-04-01-24_p_series_curated_cache_20250113/ligand_files_20250113/mcs_tanimoto_p_to_x \
+--ncpus 32
