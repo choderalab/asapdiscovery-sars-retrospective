f2b46b3e47fa1a116f65f70b2bbaa32ce6592735
Alexander Matthew Payne <alexpayne1997@gmail.com>
2025-04-04 16:31:01 -0400

    add run_evals script

--- science/20250212_x_series_docking/03_docking_analysis_scripts/10_run_evaluators_scaffold_date_split.sh
+++ science/20250212_x_series_docking/03_docking_analysis_scripts/10_run_evaluators_scaffold_date_split.sh
@@ -2,10 +2,10 @@
 #SBATCH --job-name=run_evaluators_scafold_date_split
 #SBATCH --output=logs/run_evaluators_scafold_date_split_%A_%a.out
 #SBATCH --error=logs/run_evaluators_scafold_date_split_%A_%a.err
-#SBATCH --cpus-per-task 4
+#SBATCH --cpus-per-task 32
 #SBATCH --partition=cpu
 #SBATCH --mem=16GB
-#SBATCH --time=0:30:00
+#SBATCH --time=5:00:00
 #SBATCH --array=0-9
 
 source ~/.bashrc
@@ -19,7 +19,7 @@ data_dir='/data1/choderaj/paynea/asap-datasets/20250212_p_to_x_posit'
 python3 08_run_evaluators.py \
 --input $data_dir/combined_results/20250311_combined_results \
 --output $data_dir/scaffold_split \
---n-cpus 4 \
+--n-cpus 32 \
 --job-id $SLURM_ARRAY_TASK_ID \
 --evaluator-json $data_dir/scaffold_split/evaluator_"${SLURM_ARRAY_TASK_ID}".json
 
