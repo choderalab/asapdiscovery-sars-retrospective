695925ac1a293bf32567c9340530045f5b32e725
Alexander Matthew Payne <alexpayne1997@gmail.com>
2025-04-08 12:04:02 -0400

    use python script instead

--- science/20250212_x_series_docking/03_docking_analysis_scripts/11_combine_scaffold_date_split_results.sh
+++ science/20250212_x_series_docking/03_docking_analysis_scripts/11_combine_scaffold_date_split_results.sh
@@ -13,24 +13,9 @@ conda activate harbor
 echo Start
 date
 
-# Directory containing CSV files
-input_dir="/data1/choderaj/paynea/asap-datasets/20250212_p_to_x_posit/scaffold_split"
+data_dir='/data1/choderaj/paynea/asap-datasets/20250212_p_to_x_posit/scaffold_split'
 
-# Output file
-output_file="combined_results.csv"
-
-touch "$input_dir/$output_file"
-
-# Take header from first CSV file and write to output
-head -n 1 $(find "$input_dir/*/" -name "*.csv" | head -n 1) > "$output_file"
-
-# Append all files, skipping their headers
-for file in "$input_dir/*"/*.csv; do
-    tail -n +2 "$file" >> "$input_dir/$output_file"
-done
-
-echo "Combined CSV files into $output_file"
-
+python script.py -i $data_dir -o "$data_dir"/combined_results.csv -p "*/*.csv"
 
 date
 echo Done
