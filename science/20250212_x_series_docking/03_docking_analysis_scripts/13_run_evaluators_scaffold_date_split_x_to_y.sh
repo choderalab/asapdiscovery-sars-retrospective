e7241eebb9c21f7f04df62e51039c3af0449bb5d
Alexander Matthew Payne <alexpayne1997@gmail.com>
2025-04-09 17:39:45 -0400

    try with a bunch more memory

--- science/20250212_x_series_docking/03_docking_analysis_scripts/13_run_evaluators_scaffold_date_split_x_to_y.sh
+++ science/20250212_x_series_docking/03_docking_analysis_scripts/13_run_evaluators_scaffold_date_split_x_to_y.sh
@@ -2,9 +2,9 @@
 #SBATCH --job-name=run_evaluators_scafold_date_split_x_to_y
 #SBATCH --output=logs/run_evaluators_scafold_date_split_x_to_y_%a.out
 #SBATCH --error=logs/run_evaluators_scafold_date_split_x_to_y_%a.err
-#SBATCH --cpus-per-task 0
+#SBATCH --cpus-per-task 10
 #SBATCH --partition=cpu
-#SBATCH --mem=16GB
+#SBATCH --mem=32GB
 #SBATCH --time=0:10:00
 #SBATCH --array=0-2 #822 #4
 
@@ -31,17 +31,33 @@ if [ $END_FILE -gt $TOTAL_FILES ]; then
     END_FILE=$TOTAL_FILES
 fi
 
+# Collect the file names in a list
+evaluator_files=""
 for FILE_NUM in $(seq $START_FILE $END_FILE); do
   FORMATTED_NUM=$(printf "%0d" $FILE_NUM)
   FILE_NAME="$data_dir/scaffold_split_x_to_y/evaluator_settings_scaffold_split_x_to_y_${FORMATTED_NUM}.json"
-  echo "Processing $FILE_NAME"
-  python3 08_run_evaluators.py \
-  --input $data_dir/combined_results/20250311_combined_results \
-  --output $data_dir/scaffold_split_x_to_y \
-  --n-cpus 0 \
-  --job-id $SLURM_ARRAY_TASK_ID \
-  --evaluator-json $FILE_NAME
+  evaluator_files="$evaluator_files $FILE_NAME"
 done
+
+# Pass the collected files to the --evaluator-json argument
+python3 08_run_evaluators.py \
+--input $data_dir/combined_results/20250311_combined_results \
+--output $data_dir/scaffold_split_x_to_y \
+--n-cpus 10 \
+--job-id $SLURM_ARRAY_TASK_ID \
+--evaluator-json $evaluator_files
+
+#for FILE_NUM in $(seq $START_FILE $END_FILE); do
+#  FORMATTED_NUM=$(printf "%0d" $FILE_NUM)
+#  FILE_NAME="$data_dir/scaffold_split_x_to_y/evaluator_settings_scaffold_split_x_to_y_${FORMATTED_NUM}.json"
+#  echo "Processing $FILE_NAME"
+#  python3 08_run_evaluators.py \
+#  --input $data_dir/combined_results/20250311_combined_results \
+#  --output $data_dir/scaffold_split_x_to_y \
+#  --n-cpus 10 \
+#  --job-id $SLURM_ARRAY_TASK_ID \
+#  --evaluator-json $FILE_NAME
+#done
 
 date
 echo Done
