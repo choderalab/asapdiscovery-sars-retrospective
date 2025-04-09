9bef6eb635f80d900603c377479f9305a834bf5c
Alexander Matthew Payne <alexpayne1997@gmail.com>
2025-04-09 17:00:56 -0400

    test job parallelization

--- science/20250212_x_series_docking/03_docking_analysis_scripts/13_run_evaluators_scaffold_date_split_x_to_y.sh
+++ science/20250212_x_series_docking/03_docking_analysis_scripts/13_run_evaluators_scaffold_date_split_x_to_y.sh
@@ -4,9 +4,9 @@
 #SBATCH --error=logs/run_evaluators_scafold_date_split_x_to_y_%A_%a.err
 #SBATCH --cpus-per-task 1
 #SBATCH --partition=cpu
-#SBATCH --mem=4GB
+#SBATCH --mem=16GB
 #SBATCH --time=0:10:00
-#SBATCH --array=0-28224
+#SBATCH --array=0-2 #822 #4
 
 source ~/.bashrc
 conda activate harbor
@@ -16,13 +16,32 @@ date
 
 data_dir='/data1/choderaj/paynea/asap-datasets/20250212_p_to_x_posit'
 
-python3 08_run_evaluators.py \
---input $data_dir/combined_results/20250311_combined_results \
---output $data_dir/scaffold_split_x_to_y \
---n-cpus 1 \
---job-id $SLURM_ARRAY_TASK_ID \
---evaluator-json $data_dir/scaffold_split/evaluator_*_x_to_y_*"${SLURM_ARRAY_TASK_ID}".json
+# Number of files to process per job
+FILES_PER_JOB=10
 
+# Calculate the starting and ending file numbers for this job
+START_FILE=$((SLURM_ARRAY_TASK_ID * FILES_PER_JOB + 1))
+END_FILE=$((START_FILE + FILES_PER_JOB - 1))
+
+# Make sure we don't exceed the total number of files
+if [ $END_FILE -gt $TOTAL_FILES ]; then
+    END_FILE=$TOTAL_FILES
+fi
+
+echo "Processing files $START_FILE to $END_FILE (Job array ID: $SLURM_ARRAY_TASK_ID)"
+
+# Loop through the files assigned to this job
+for FILE_NUM in $(seq $START_FILE $END_FILE); do
+  FORMATTED_NUM=$(printf "%0d" $FILE_NUM)
+  FILE_NAME="$data_dir/scaffold_split/evaluator_settings_scaffold_split_x_to_y_${FORMATTED_NUM}.json"
+  echo Running $FILE_NAME
+  python3 08_run_evaluators.py \
+  --input $data_dir/combined_results/20250311_combined_results \
+  --output $data_dir/scaffold_split_x_to_y \
+  --n-cpus 1 \
+  --job-id $SLURM_ARRAY_TASK_ID \
+  --evaluator-json $FILE_NAME
+done
 
 date
 echo Done
