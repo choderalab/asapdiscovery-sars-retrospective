bc59cbc580268d3ff298a832d8b43fc0b70c40bc
Alexander Matthew Payne <alexpayne1997@gmail.com>
2025-04-09 17:07:25 -0400

    try parallelization within the python script

--- science/20250212_x_series_docking/03_docking_analysis_scripts/13_run_evaluators_scaffold_date_split_x_to_y.sh
+++ science/20250212_x_series_docking/03_docking_analysis_scripts/13_run_evaluators_scaffold_date_split_x_to_y.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=run_evaluators_scafold_date_split_x_to_y
 #SBATCH --output=logs/run_evaluators_scafold_date_split_x_to_y_%A_%a.out
 #SBATCH --error=logs/run_evaluators_scafold_date_split_x_to_y_%A_%a.err
-#SBATCH --cpus-per-task 1
+#SBATCH --cpus-per-task 10
 #SBATCH --partition=cpu
 #SBATCH --mem=16GB
 #SBATCH --time=0:10:00
@@ -31,19 +31,22 @@ if [ $END_FILE -gt $TOTAL_FILES ]; then
     END_FILE=$TOTAL_FILES
 fi
 
-echo "Processing files $START_FILE to $END_FILE (Job array ID: $SLURM_ARRAY_TASK_ID)"
-
-# Loop through the files assigned to this job
+# Collect the file names in a list
+evaluator_files=""
 for FILE_NUM in $(seq $START_FILE $END_FILE); do
   FORMATTED_NUM=$(printf "%0d" $FILE_NUM)
   FILE_NAME="$data_dir/scaffold_split_x_to_y/evaluator_settings_scaffold_split_x_to_y_${FORMATTED_NUM}.json"
-  echo Running $FILE_NAME
-  python3 08_run_evaluators.py \
-  --input $data_dir/combined_results/20250311_combined_results \
-  --output $data_dir/scaffold_split_x_to_y \
-  --n-cpus 1 \
-  --job-id $SLURM_ARRAY_TASK_ID \
-  --evaluator-json $FILE_NAME
+  evaluator_files="$evaluator_files $FILE_NAME"
+done
+
+echo "Processing evaluator files: $evaluator_files"
+
+python3 08_run_evaluators.py \
+--input $data_dir/combined_results/20250311_combined_results \
+--output $data_dir/scaffold_split_x_to_y \
+--n-cpus 10 \
+--job-id $SLURM_ARRAY_TASK_ID \
+--evaluator-json $evaluator_files
 done
 
 date
