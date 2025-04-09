867cab51bb6d738092207c0542abd55f66c89f9d
Alexander Matthew Payne <alexpayne1997@gmail.com>
2025-04-09 16:47:11 -0400

    update evaluator runner

--- science/20250212_x_series_docking/03_docking_analysis_scripts/13_run_evaluators_scaffold_date_split_x_to_y.sh
+++ science/20250212_x_series_docking/03_docking_analysis_scripts/13_run_evaluators_scaffold_date_split_x_to_y.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=run_evaluators_scafold_date_split
-#SBATCH --output=logs/run_evaluators_scafold_date_split_%A_%a.out
-#SBATCH --error=logs/run_evaluators_scafold_date_split_%A_%a.err
-#SBATCH --cpus-per-task 4
+#SBATCH --job-name=run_evaluators_scafold_date_split_x_to_y
+#SBATCH --output=logs/run_evaluators_scafold_date_split_x_to_y_%A_%a.out
+#SBATCH --error=logs/run_evaluators_scafold_date_split_x_to_y_%A_%a.err
+#SBATCH --cpus-per-task 1
 #SBATCH --partition=cpu
-#SBATCH --mem=16GB
-#SBATCH --time=0:30:00
-#SBATCH --array=0-1585
+#SBATCH --mem=4GB
+#SBATCH --time=0:10:00
+#SBATCH --array=0-28224
 
 source ~/.bashrc
 conda activate harbor
@@ -18,8 +18,8 @@ data_dir='/data1/choderaj/paynea/asap-datasets/20250212_p_to_x_posit'
 
 python3 08_run_evaluators.py \
 --input $data_dir/combined_results/20250311_combined_results \
---output $data_dir/scaffold_split \
---n-cpus 4 \
+--output $data_dir/scaffold_split_x_to_y \
+--n-cpus 1 \
 --job-id $SLURM_ARRAY_TASK_ID \
 --evaluator-json $data_dir/scaffold_split/evaluator_*_x_to_y_*"${SLURM_ARRAY_TASK_ID}".json
 
