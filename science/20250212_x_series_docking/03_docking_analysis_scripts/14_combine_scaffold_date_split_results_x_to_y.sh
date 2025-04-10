0c5db4117d36f160c26ab9e336d3a9853306e607
Alexander Matthew Payne <alexpayne1997@gmail.com>
2025-04-10 11:50:50 -0400

    combine x_to_y scaffold split

---
+++ science/20250212_x_series_docking/03_docking_analysis_scripts/14_combine_scaffold_date_split_results_x_to_y.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=combine_scafold_date_split_results_x_to_y
+#SBATCH --output=logs/combine_scafold_date_split_results_x_to_y.out
+#SBATCH --error=logs/combine_scafold_date_split_results_x_to_y.err
+#SBATCH --cpus-per-task 1
+#SBATCH --partition=cpu
+#SBATCH --mem=16GB
+#SBATCH --time=0:30:00
+
+source ~/.bashrc
+conda activate harbor
+
+echo Start
+date
+
+data_dir='/data1/choderaj/paynea/asap-datasets/20250212_p_to_x_posit/scaffold_split_x_to_y'
+
+python combine_date_split_results.py -i $data_dir -o "$data_dir"/combined_results.csv -p "*/*.csv"
+
+date
+echo Done
