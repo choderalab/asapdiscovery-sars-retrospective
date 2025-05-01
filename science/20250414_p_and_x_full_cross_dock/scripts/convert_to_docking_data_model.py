230eff720fe3594f6bc92817c2b4edb9679ed9e9
Alexander Matthew Payne <alexpayne1997@gmail.com>
2025-05-01 05:15:30 -0400

    implement evaluator factory and tests

--- science/20250414_p_and_x_full_cross_dock/scripts/convert_to_docking_data_model.py
+++ science/20250414_p_and_x_full_cross_dock/scripts/convert_to_docking_data_model.py
@@ -25,11 +25,11 @@ def main(
     df = pd.read_csv(input_csv)
 
     # construct Data
-    pose_data = DataFrameModel(dataframe=df, type=DataFrameType.POSE, key_columns=["Query_Ligand", "Reference_Structure", "Pose_ID"])
-    ref_data = DataFrameModel(dataframe=df, type=DataFrameType.REFERENCE, key_columns=["Reference_Structure"])
-    lig_data = DataFrameModel(dataframe=df, type=DataFrameType.QUERY, key_columns=["Query_Structure"])
-    similarity_data = DataFrameModel(dataframe=df, type=DataFrameType.CHEMICAL_SIMILARITY, key_columns=["Query_Structure", "Reference_Structure", "Aligned", "radius", "bitsize", "fingerprint"])
-    scaffold_data = DataFrameModel(dataframe=df, type=DataFrameType.CHEMICAL_SIMILARITY, key_columns=["Query_Ligand", "Reference_Ligand"])
+    pose_data = DataFrameModel(dataframe=df, name="PoseData", type=DataFrameType.POSE, key_columns=["Query_Ligand", "Reference_Structure", "Pose_ID"])
+    ref_data = DataFrameModel(dataframe=df, name="RefData", type=DataFrameType.REFERENCE, key_columns=["Reference_Structure"])
+    lig_data = DataFrameModel(dataframe=df, name="LigData", type=DataFrameType.QUERY, key_columns=["Query_Structure"])
+    similarity_data = DataFrameModel(dataframe=df, name="SimilarityData", type=DataFrameType.CHEMICAL_SIMILARITY, key_columns=["Query_Structure", "Reference_Structure", "Aligned", "radius", "bitsize", "fingerprint"])
+    scaffold_data = DataFrameModel(dataframe=df, name="ScaffoldData", type=DataFrameType.CHEMICAL_SIMILARITY, key_columns=["Query_Ligand", "Reference_Ligand"])
     data = DockingDataModel.from_models([pose_data, ref_data, lig_data, similarity_data, scaffold_data])
 
     data.serialize(output_file_prefix)
