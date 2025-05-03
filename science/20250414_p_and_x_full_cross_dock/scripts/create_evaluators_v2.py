af952df59cc04fe26cca449df16a22100353a7f6
Alexander Matthew Payne <alexpayne1997@gmail.com>
2025-05-03 17:46:11 -0400

    turn off caching for create evals

--- science/20250414_p_and_x_full_cross_dock/scripts/create_evaluators_v2.py
+++ science/20250414_p_and_x_full_cross_dock/scripts/create_evaluators_v2.py
@@ -4,7 +4,11 @@ This script creates the calculations that will be run on a cross-docking dataset
 
 import click
 from pathlib import Path
-from harbor.analysis.cross_docking import DockingDataModel, EvaluatorFactory
+from harbor.analysis.cross_docking import (
+    DockingDataModel,
+    EvaluatorFactory,
+    ScaffoldSplitOptions,
+)
 from harbor.analysis.utils import FileLogger
 
 
@@ -70,7 +74,9 @@ def main(input_parquet, output):
     evf.pairwise_split_settings.scaffold_split_settings.use = True
     evf.pairwise_split_settings.scaffold_split_settings.reference_scaffold_min_count = 5
     evf.pairwise_split_settings.scaffold_split_settings.query_scaffold_min_count = 5
-    evf.pairwise_split_settings.scaffold_split_settings.scaffold_split_option = "x_to_y"
+    evf.pairwise_split_settings.scaffold_split_settings.scaffold_split_option = (
+        ScaffoldSplitOptions.X_TO_Y
+    )
     evf.scorer_settings.rmsd_scorer_settings.use = True
     evf.scorer_settings.posit_scorer_settings.use = True
     save_and_create_evs(evf, data, name, output, logger)
@@ -82,7 +88,9 @@ def main(input_parquet, output):
     evf.pairwise_split_settings.scaffold_split_settings.use = True
     evf.pairwise_split_settings.scaffold_split_settings.reference_scaffold_min_count = 5
     evf.pairwise_split_settings.scaffold_split_settings.query_scaffold_min_count = 5
-    evf.pairwise_split_settings.scaffold_split_settings.scaffold_split_option = "x_to_y"
+    evf.pairwise_split_settings.scaffold_split_settings.scaffold_split_option = (
+        ScaffoldSplitOptions.X_TO_Y
+    )
     evf.scorer_settings.rmsd_scorer_settings.use = True
     evf.scorer_settings.posit_scorer_settings.use = True
     evf.combine_reference_and_similarity_splits = True
